87b051bcd01195fb0b2fa12aec4d0d5dd1fb1350
Opilane TTHK <opilane@tthk.ee>
2021-08-31 12:31:24 +0300

    Add project files.

---
+++ Heroes/Hero.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes
+{
+    public class Hero
+    {
+        public string Name { get; private set; }
+        public string Location { get; private set; }
+
+        public Hero(string name, string location)
+        {
+            Name = name;
+            Location = location;
+        }
+
+        public int Rescue(int count)
+        {
+            return Convert.ToInt32(Math.Round(count * 0.95));
+        }
+
+        public string toString(int count)
+        {
+            return "Hero " + Name  + "has rescured " + Rescue(100) + "% people in" + Location + " city.";
+        }
+    }
+
+    public class Superhero : Hero
+    {
+        private double Agility { get; set; }
+
+        public Superhero(string name, string location) : base(name, location)
+        {
+            Agility = new Random().Next(1, 6);
+        }
+
+        public int Rescue(int count)
+        {
+            return Convert.ToInt32(Math.Round(count * (0.95 + Agility / 100)));
+        }
+
+        public string toString(int count)
+        {
+            return "Superhero " + Name.Split('*')[0] + " has rescured " + Rescue(count) + "% people in" + Location + " city.";
+        }
+    }
+}
---
+++ Heroes/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HeroesSeperation(@"C:\Users\opilane\Source\Repos\Heroes\Heroes\Heroes.txt");
+            Console.WriteLine("- - - - -");
+            foreach (var el in HeroesList)
+            {
+                Console.WriteLine("Name : " + el[0] + "\nLocation : " + el[1] + "\n- - - - -");
+            }
+            Superhero superhero = new Superhero(HeroesList[0][0], HeroesList[0][1]);
+            Console.WriteLine(superhero.toString(100));
+            Hero hero = new Hero(HeroesList[1][0], HeroesList[1][1]);
+            Console.WriteLine(hero.toString(50));
+            Console.ReadLine();
+        }
+
+        static private List<string[]> HeroesList = new List<string[]>();
+        static private void HeroesSeperation(string path)
+        {
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] seperated = line.Split('/');
+                    string name;
+                    string location;
+
+
+                    if (line.Contains("*"))
+                    {
+                        Superhero superhero = new Superhero(seperated[0], seperated[1]);
+                        name = superhero.Name.Replace('*', ' ');
+                        location = superhero.Location;
+                    }
+                    else
+                    {
+                        Hero hero = new Hero(seperated[0], seperated[1]);
+                        name = hero.Name;
+                        location = hero.Location;
+                    }
+                    HeroesList.Add(new string[] { name, location });
+                }
+            }
+        }
+    }
+}
